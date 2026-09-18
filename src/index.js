a7f9dabb4c5d5b8fd8d42104d9f9c052e6afc2ab
nexsys-innovation <nexsys.np@gmail.com>
2026-09-18 23:45:44 +0545

    payment and others

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
 
+//payment and others
 createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>);
